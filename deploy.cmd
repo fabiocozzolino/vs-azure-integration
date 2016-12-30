fcfe7973b89fa91cdf369fede7d38fc0bdc90679
fabiocozzolino <fabio@MacBook-Air-di-FabioMac.local>
2016-12-30 18:40:04 +0100

    update deploy

--- deploy.cmd
+++ deploy.cmd
@@ -66,10 +66,6 @@ SET MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 
 echo Handling ASP.NET Core Web Application deployment.
 
-:: 1. Restore nuget packages
-:: call :ExecuteCmd dotnet restore "XConn\XConn.sln"
-:: IF !ERRORLEVEL! NEQ 0 goto error
-
 :: 2. Build and publish
 call :ExecuteCmd dotnet publish "XConn\MobileAppService\XConn.MobileAppService.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
 IF !ERRORLEVEL! NEQ 0 goto error
